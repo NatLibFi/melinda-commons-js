5095e1fda00c25fab29215a206373cf97d5bdd28
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2019-01-21 11:04:59 +0200

    Change license & add new function 1) Change license to LGPL 2) Add readEnvironmentVariable function

--- src/utils.js
+++ src/utils.js
@@ -9,22 +9,25 @@
 * This file is part of melinda-commons-js
 *
 * melinda-commons-js program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
+* it under the terms of the GNU Lesser General Public License as
 * published by the Free Software Foundation, either version 3 of the
 * License, or (at your option) any later version.
 *
 * melinda-commons-js is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Affero General Public License for more details.
+* GNU Lesser General Public License for more details.
 *
-* You should have received a copy of the GNU Affero General Public License
+* You should have received a copy of the GNU Lesser General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *
 * @licend  The above is the entire license notice
 * for the JavaScript code in this file.
 *
 */
+
+// Needed for Rewire to work
+import console from 'console';
 
 export function createAuthorizationHeader(username, password = '') {
 	const encoded = Buffer.from(`${username}:${password}`).toString('base64');
@@ -35,4 +38,17 @@ export function isDeletedRecord(record) {
 	return record
 		.get(/^STA$/)
 		.some(f => f.subfields.some(sf => sf.code === 'a' && sf.value === 'DELETED'));
+}
+
+export function readEnvironmentVariable(name, defaultValue, opts = {}) {
+	if (process.env[name] === undefined) {
+		if (defaultValue === undefined) {
+			throw new Error(`Mandatory environment variable missing: ${name}`);
+		}
+
+		const loggedDefaultValue = opts.hideDefaultValue ? '[hidden]' : defaultValue;
+		console.log(`No environment variable set for ${name}, using default value: ${loggedDefaultValue}`);
+	}
+
+	return process.env[name] || defaultValue;
 }
--- src/utils.spec.js
+++ src/utils.spec.js
@@ -9,16 +9,16 @@
 * This file is part of melinda-commons-js
 *
 * melinda-commons-js program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
+* it under the terms of the GNU Lesser General Public License as
 * published by the Free Software Foundation, either version 3 of the
 * License, or (at your option) any later version.
 *
 * melinda-commons-js is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Affero General Public License for more details.
+* GNU Lesser General Public License for more details.
 *
-* You should have received a copy of the GNU Affero General Public License
+* You should have received a copy of the GNU Lesser General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *
 * @licend  The above is the entire license notice
@@ -26,13 +26,13 @@
 *
 */
 
-/* eslint-disable camelcase */
+/* eslint-disable camelcase, import/named */
 
 import fs from 'fs';
 import path from 'path';
 import {expect} from 'chai';
 import {MarcRecord} from '@natlibfi/marc-record';
-import * as testContext from './utils';
+import {createAuthorizationHeader, isDeletedRecord, readEnvironmentVariable, __RewireAPI__ as RewireAPI} from './utils';
 
 MarcRecord.setValidationOptions({subfieldValues: false});
 
@@ -44,7 +44,7 @@ const isDeletedRecord_record2 = fs.readFileSync(path.join(FIXTURES_PATH, 'isDele
 describe('utils', () => {
 	describe('createAuthorizationHeader', () => {
 		it('Should create a proper Authorization header', () => {
-			const value = testContext.createAuthorizationHeader('foo', 'bar');
+			const value = createAuthorizationHeader('foo', 'bar');
 			expect(value).to.equal('Basic Zm9vOmJhcg==');
 		});
 	});
@@ -52,12 +52,46 @@ describe('utils', () => {
 	describe('isDeletedRecord', () => {
 		it('Should find the record deleted', () => {
 			const record = new MarcRecord(JSON.parse(isDeletedRecord_record1));
-			expect(testContext.isDeletedRecord(record)).to.equal(true);
+			expect(isDeletedRecord(record)).to.equal(true);
 		});
 
 		it('Should find the record not deleted', () => {
 			const record = new MarcRecord(JSON.parse(isDeletedRecord_record2));
-			expect(testContext.isDeletedRecord(record)).to.equal(false);
+			expect(isDeletedRecord(record)).to.equal(false);
+		});
+	});
+
+	describe('readEnvironmentVariable', () => {
+		afterEach(() => {
+			RewireAPI.__ResetDependency__('console');
+			delete process.env.FOO;
+		});
+
+		it('Should read a environment variable', () => {
+			process.env.FOO = 'bar';
+			expect(readEnvironmentVariable('FOO')).to.equal('bar');
+		});
+
+		it('Should use  a default value for environment', () => {
+			RewireAPI.__Rewire__('console', {log: msg => {
+				expect(msg).to.equal('No environment variable set for FOO, using default value: fubar');
+			}});
+
+			expect(readEnvironmentVariable('FOO', 'fubar')).to.equal('fubar');
+		});
+
+		it('Should not log the default value', () => {
+			RewireAPI.__Rewire__('console', {log: msg => {
+				expect(msg).to.equal('No environment variable set for FOO, using default value: [hidden]');
+			}});
+
+			expect(readEnvironmentVariable('FOO', 'fubar', {hideDefaultValue: true})).to.equal('fubar');
+		});
+
+		it('Should throw because mandatory variable is missing', () => {
+			expect(() => {
+				readEnvironmentVariable('FOO');
+			}).to.throw(Error, /^Mandatory environment variable missing: FOO$/);
 		});
 	});
 });
