b06e2c4913d3b07ff70e81b61195b148628d2e0b
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-05-14 07:41:06 +0300

    Added some logging and fix ApiError

--- src/api-client.js
+++ src/api-client.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 import httpStatus from 'http-status';
 import {URL} from 'url';
 import {createLogger} from './utils';
-import {ApiError} from './error';
+import {Error as ApiError} from './error';
 
 export function createApiClient({restApiUrl, restApiUsername, restApiPassword, userAgent = 'Melinda commons API client / Javascript'}) {
     const logger = createLogger();
@@ -13,10 +13,12 @@ export function createApiClient({restApiUrl, restApiUsername, restApiPassword, u
     };
 
     async function getRecord(recordId) {
+        logger.log('verbose', 'Getting record');
         return doRequest({method: 'get', path: recordId});
     }
 
     async function postPrio({params, contentType, body}, id = false) {
+        logger.log('verbose', 'Posting prio');
         if (id) {
             return doRequest({method: 'post', path: id, params, contentType, body});
         }
@@ -25,28 +27,33 @@ export function createApiClient({restApiUrl, restApiUsername, restApiPassword, u
     }
 
     async function postBulk({params, contentType, body}) {
+        logger.log('verbose', 'Posting bulk');
         return doRequest({method: 'post', path: 'bulk/', params, contentType, body});
     }
 
     async function getMetadata({id}) {
+        logger.log('verbose', 'Getting metadata');
         return doRequest({method: 'get', path: 'bulk/', params: {id}});
     }
 
     async function getStatus({id}) {
+        logger.log('verbose', 'Getting status');
         const result = await getMetadata({id});
         return result.queueItemState;
     }
 
     async function deleteBulk({id}) {
+        logger.log('verbose', 'Deleting bulk');
         return doRequest({method: 'delete', path: 'bulk/', params: {id}});
     }
 
     async function doRequest({method, path, params = false, contentType = 'application/json', body = null}) {
         try {
+            logger.log('verbose', 'Doing request');
             const query = params ? new URLSearchParams(params) : '';
             const url = new URL(`${restApiUrl}${path}${query === '' ? '' : '?'}${query}`);
 
-            logger.log('silly', url.toString());
+            logger.log('silly', `connection URL ${url.toString()}`);
 
             const response = await fetch(url, {
                 method,
