ffecdc4f0d7f977cea049904e42ccc07dbd84a9b
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-08-18 16:39:54 +0300

    fix lint

--- src/subRecordPicker.js
+++ src/subRecordPicker.js
@@ -3,21 +3,21 @@ import {MARCXML} from '@natlibfi/marc-record-serializers';
 import {createLogger} from './utils';
 
 export function createSubrecordPicker(sruUrl) {
-	const logger = createLogger();
-	const sruClient = createSruClient({url: sruUrl, recordSchema: 'marcxml'});
+  const logger = createLogger();
+  const sruClient = createSruClient({url: sruUrl, recordSchema: 'marcxml'});
 
-	return {readSubrecords};
+  return {readSubrecords};
 
-	function readSubrecords(recordId) {
-		logger.log('verbose', `Picking subrecords for ${recordId}`);
-		return new Promise((resolve, reject) => {
-			const records = [];
-			sruClient.searchRetrieve(`melinda.partsofhost=${recordId}`)
-				.on('record', xmlString => {
-					records.push(MARCXML.from(xmlString));
-				})
-				.on('end', () => resolve(records))
-				.on('error', err => reject(err));
-		});
-	}
+  function readSubrecords(recordId) {
+    logger.log('verbose', `Picking subrecords for ${recordId}`);
+    return new Promise((resolve, reject) => {
+      const records = [];
+      sruClient.searchRetrieve(`melinda.partsofhost=${recordId}`)
+        .on('record', xmlString => {
+          records.push(MARCXML.from(xmlString)); // eslint-disable-line functional/immutable-data
+        })
+        .on('end', () => resolve(records))
+        .on('error', err => reject(err));
+    });
+  }
 }
