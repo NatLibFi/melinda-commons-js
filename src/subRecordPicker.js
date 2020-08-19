c7d05cef8774365aef3515f8d5182543a56dc0f2
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-08-19 12:39:20 +0300

    read some and read all subrecords

--- src/subRecordPicker.js
+++ src/subRecordPicker.js
@@ -3,12 +3,28 @@ import {MARCXML} from '@natlibfi/marc-record-serializers';
 import createDebugLogger from 'debug';
 
 export function createSubrecordPicker(sruUrl, retrieveAll = false) {
-  const sruClient = createSruClient({url: sruUrl, recordSchema: 'marcxml', retrieveAll});
   const debug = createDebugLogger('@natlibfi/melinda-commons:subRecordPicker');
+  debug(`SRU client url: ${sruUrl}`);
+  const sruClient = createSruClient({url: sruUrl, recordSchema: 'marcxml', retrieveAll});
 
-  return {readSubrecords};
+  return {readSomeSubrecords, readAllSubrecords};
 
-  function readSubrecords(recordId) {
+  function readSomeSubrecords(recordId, offset = 0) {
+    debug(`Picking subrecords for ${recordId}`);
+    return new Promise((resolve, reject) => {
+      const records = [];
+      sruClient.searchRetrieve(`melinda.partsofhost=${recordId}`, {startRecord: offset})
+        .on('record', xmlString => {
+          records.push(MARCXML.from(xmlString)); // eslint-disable-line functional/immutable-data
+        })
+        .on('end', nextRecordOffset => {
+          resolve({nextRecordOffset, records});
+        })
+        .on('error', err => reject(err));
+    });
+  }
+
+  function readAllSubrecords(recordId) {
     debug(`Picking subrecords for ${recordId}`);
     return new Promise((resolve, reject) => {
       const records = [];
@@ -16,11 +32,7 @@ export function createSubrecordPicker(sruUrl, retrieveAll = false) {
         .on('record', xmlString => {
           records.push(MARCXML.from(xmlString)); // eslint-disable-line functional/immutable-data
         })
-        .on('end', nextRecordOffset => {
-          if (nextRecordOffset) {
-            return resolve({nextRecordOffset, records});
-          }
-
+        .on('end', () => {
           resolve(records);
         })
         .on('error', err => reject(err));
