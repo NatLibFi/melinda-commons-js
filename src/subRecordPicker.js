451121e8c48b4efd436d35efbcd5ddcd5177e113
Joni Ollila <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-09-11 11:45:27 +0300

    Add subrecord picker tests (#28)

--- src/subRecordPicker.js
+++ src/subRecordPicker.js
@@ -1,24 +1,34 @@
 import createSruClient from '@natlibfi/sru-client';
 import {MARCXML} from '@natlibfi/marc-record-serializers';
 import createDebugLogger from 'debug';
+import {Error as ApiError} from './error';
 
 export function createSubrecordPicker(sruUrl, retrieveAll = false) {
+  if (sruUrl === undefined) { // eslint-disable-line functional/no-conditional-statement
+    throw new ApiError(400, 'Invalid sru url');
+  }
+
   const debug = createDebugLogger('@natlibfi/melinda-commons:subRecordPicker');
   debug(`SRU client url: ${sruUrl}`);
   const sruClient = createSruClient({url: sruUrl, recordSchema: 'marcxml', retrieveAll});
 
   return {readSomeSubrecords, readAllSubrecords};
 
-  function readSomeSubrecords(recordId, offset = 0) {
+  function readSomeSubrecords(recordId, offset = 1) {
     debug(`Picking subrecords for ${recordId}`);
     return new Promise((resolve, reject) => {
-      const records = [];
+      const promises = [];
       sruClient.searchRetrieve(`melinda.partsofhost=${recordId}`, {startRecord: offset})
         .on('record', xmlString => {
-          records.push(MARCXML.from(xmlString)); // eslint-disable-line functional/immutable-data
+          promises.push(MARCXML.from(xmlString, {subfieldValues: false})); // eslint-disable-line functional/immutable-data
         })
-        .on('end', nextRecordOffset => {
-          resolve({nextRecordOffset, records});
+        .on('end', async nextRecordOffset => {
+          try {
+            const records = await Promise.all(promises);
+            resolve({nextRecordOffset, records});
+          } catch (error) {
+            reject(error);
+          }
         })
         .on('error', err => reject(err));
     });
@@ -27,13 +37,18 @@ export function createSubrecordPicker(sruUrl, retrieveAll = false) {
   function readAllSubrecords(recordId) {
     debug(`Picking subrecords for ${recordId}`);
     return new Promise((resolve, reject) => {
-      const records = [];
+      const promises = [];
       sruClient.searchRetrieve(`melinda.partsofhost=${recordId}`)
         .on('record', xmlString => {
-          records.push(MARCXML.from(xmlString)); // eslint-disable-line functional/immutable-data
+          promises.push(MARCXML.from(xmlString, {subfieldValues: false})); // eslint-disable-line functional/immutable-data
         })
-        .on('end', () => {
-          resolve(records);
+        .on('end', async () => {
+          try {
+            const records = await Promise.all(promises);
+            resolve({records});
+          } catch (error) {
+            reject(error);
+          }
         })
         .on('error', err => reject(err));
     });
---
+++ src/subRecordPicker.spec.js
@@ -0,0 +1,76 @@
+/**
+*
+* @licstart  The following is the entire license notice for the JavaScript code in this file.
+*
+* Shared modules for Melinda's software
+*
+* Copyright (C) 2018-2020 University Of Helsinki (The National Library Of Finland)
+*
+* This file is part of melinda-commons-js
+*
+* melinda-commons-js program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* melinda-commons-js is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* @licend  The above is the entire license notice
+* for the JavaScript code in this file.
+*
+*/
+
+import {expect} from 'chai';
+import {createSubrecordPicker} from './subRecordPicker';
+import {READERS} from '@natlibfi/fixura';
+import generateTests from '@natlibfi/fixugen-http-client';
+import {MarcRecord} from '@natlibfi/marc-record';
+
+describe('subRecordPicker', () => {
+  const sruUrl = 'https://sru';
+  const retrieveAll = false;
+
+  describe('createSubrecordPicker', () => {
+    it('Client should have methods', () => {
+      const client = createSubrecordPicker(sruUrl, retrieveAll);
+      expect(client).to.respondTo('readAllSubrecords');
+      expect(client).to.respondTo('readSomeSubrecords');
+    });
+
+    it('Create client should work with just one parametter', () => {
+      const client = createSubrecordPicker(sruUrl);
+      expect(client).to.respondTo('readAllSubrecords');
+      expect(client).to.respondTo('readSomeSubrecords');
+    });
+
+    it('Create client should have at least one parametter', () => {
+      expect(createSubrecordPicker).to.throw();
+    });
+  });
+
+  generateTests({
+    callback,
+    path: [__dirname, '..', 'test-fixtures', 'subRecordPicker']
+  });
+
+  async function callback({getFixture, method, sruUrl, retrieveAll, recordId}) {
+    const client = createSubrecordPicker(sruUrl, retrieveAll);
+    const expectedRecords = getFixture({components: ['expected-records.json'], reader: READERS.JSON});
+    const {records} = await client[method](recordId);
+
+    // Expect records to be marc records
+    const [record] = records;
+    expect(record).to.be.instanceof(MarcRecord);
+    expect(format()).to.eql(expectedRecords);
+
+    function format() {
+      return records.map(r => r.toObject());
+    }
+  }
+});
