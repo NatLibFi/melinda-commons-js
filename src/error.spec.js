65051628210b83dde3ea3457040f5b4391c97b9e
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-10-05 11:34:07 +0300

    add test file

---
+++ src/error.spec.js
@@ -0,0 +1,43 @@
+
+import {expect} from 'chai';
+import CommonError from './error';
+
+describe('error', () => {
+  const doThrow = () => {
+    throw new CommonError(400, 'Test error!');
+  };
+  it('Should throw error', () => {
+    expect(doThrow).to.throw(CommonError);
+  });
+  it('Should contain status', () => {
+    try {
+      doThrow();
+    } catch (error) {
+      expect(error).to.have.property('status');
+      expect(error.status).to.eql(400);
+    }
+  });
+  it('Should contain payload', () => {
+    try {
+      doThrow();
+    } catch (error) {
+      expect(error).to.have.property('payload');
+      expect(error.payload).to.eql('Test error!');
+    }
+  });
+  it('Should contain stack', () => {
+    try {
+      doThrow();
+    } catch (error) {
+      expect(error).to.have.property('stack');
+    }
+  });
+  it('Should contain function logError', () => {
+    try {
+      doThrow();
+    } catch (error) {
+      expect(error).to.have.property('logError');
+    }
+  });
+});
+
