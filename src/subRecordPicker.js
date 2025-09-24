6b901f75ad2865010de32d5b7278bde015bfbedd
Minttu Hurme <ammsalme@users.noreply.github.com>
2025-09-24 18:02:25 +0300

    Add readSubrecordAmount and some tests

--- src/subRecordPicker.js
+++ src/subRecordPicker.js
@@ -3,29 +3,56 @@ import {MARCXML} from '@natlibfi/marc-record-serializers';
 import createDebugLogger from 'debug';
 import {Error as ApiError} from './error';
 
-export function createSubrecordPicker(sruUrl, retrieveAll = false) {
+const componentIndex = 'melinda.partsofhost';
+const monoHostComponentIndex = 'melinda.partsofmonohost';
+
+export function createSubrecordPicker(sruUrl, retrieveAll = false, monoHostComponentsOnly = false) {
+
+  const debug = createDebugLogger('@natlibfi/melinda-commons:subRecordPicker');
+
+  debug(`SRU client url: ${sruUrl}`);
   if (sruUrl === undefined) { // eslint-disable-line functional/no-conditional-statements
     throw new ApiError(400, 'Invalid sru url');
   }
 
-  const debug = createDebugLogger('@natlibfi/melinda-commons:subRecordPicker');
-  debug(`SRU client url: ${sruUrl}`);
   const sruClient = createSruClient({url: sruUrl, recordSchema: 'marcxml', retrieveAll});
+  // SRU client with maxRecordsPerRequest does not retrieve any records
+  const sruClientForAmount = createSruClient({url: sruUrl, recordSchema: 'marcxml', retrieveAll, maxRecordsPerRequest: 0});
 
-  return {readSomeSubrecords, readAllSubrecords};
+  const index = monoHostComponentsOnly ? monoHostComponentIndex : componentIndex;
+  debug(`Using index ${index}, (monoHostComponentsOnly: ${monoHostComponentsOnly})`);
+
+  return {readSubrecordAmount, readSomeSubrecords, readAllSubrecords};
+
+  function readSubrecordAmount(recordId) {
+    debug(`Getting subrecord amount for ${recordId}`);
+    return new Promise((resolve, reject) => {
+      sruClientForAmount.searchRetrieve(`${index}=${recordId}`)
+        .on('total', totalNumberOfRecords => {
+          resolve({amount: totalNumberOfRecords});
+        })
+        .on('error', err => reject(err));
+    });
+  }
 
   function readSomeSubrecords(recordId, offset = 1) {
     debug(`Picking subrecords for ${recordId}`);
     return new Promise((resolve, reject) => {
       const promises = [];
-      sruClient.searchRetrieve(`melinda.partsofhost=${recordId}`, {startRecord: offset})
+      // eslint-disable-next-line functional/no-let
+      let amount;
+      sruClient.searchRetrieve(`${index}=${recordId}`, {startRecord: offset})
+        .on('total', totalNumberOfRecords => {
+          // eslint-disable-next-line no-const-assign
+          amount = totalNumberOfRecords;
+        })
         .on('record', xmlString => {
           promises.push(MARCXML.from(xmlString, {subfieldValues: false})); // eslint-disable-line functional/immutable-data
         })
         .on('end', async nextRecordOffset => {
           try {
             const records = await Promise.all(promises);
-            resolve({nextRecordOffset, records});
+            resolve({nextRecordOffset, records, amount});
           } catch (error) {
             reject(error);
           }
@@ -38,14 +65,20 @@ export function createSubrecordPicker(sruUrl, retrieveAll = false) {
     debug(`Picking subrecords for ${recordId}`);
     return new Promise((resolve, reject) => {
       const promises = [];
-      sruClient.searchRetrieve(`melinda.partsofhost=${recordId}`)
+      // eslint-disable-next-line functional/no-let
+      let amount;
+      sruClient.searchRetrieve(`${index}=${recordId}`)
+        .on('total', totalNumberOfRecords => {
+          // eslint-disable-next-line no-const-assign
+          amount = totalNumberOfRecords;
+        })
         .on('record', xmlString => {
           promises.push(MARCXML.from(xmlString, {subfieldValues: false})); // eslint-disable-line functional/immutable-data
         })
         .on('end', async () => {
           try {
             const records = await Promise.all(promises);
-            resolve({records});
+            resolve({records, amount});
           } catch (error) {
             reject(error);
           }
--- src/subRecordPicker.spec.js
+++ src/subRecordPicker.spec.js
@@ -2,22 +2,29 @@ import {expect} from 'chai';
 import {createSubrecordPicker} from './subRecordPicker';
 import {READERS} from '@natlibfi/fixura';
 import generateTests from '@natlibfi/fixugen-http-client';
+import createDebugLogger from 'debug';
+
+const debug = createDebugLogger('@natlibfi/melinda-commons:subRecordPicker:test');
+
 
 describe('subRecordPicker', () => {
   const sruUrl = 'https://sru';
   const retrieveAll = false;
+  //const monoHostComponentsOnly = false;
 
   describe('createSubrecordPicker', () => {
     it('Client should have methods', () => {
       const client = createSubrecordPicker(sruUrl, retrieveAll);
       expect(client).to.respondTo('readAllSubrecords');
       expect(client).to.respondTo('readSomeSubrecords');
+      expect(client).to.respondTo('readSubrecordAmount');
     });
 
     it('Create client should work with just one parametter', () => {
       const client = createSubrecordPicker(sruUrl);
       expect(client).to.respondTo('readAllSubrecords');
       expect(client).to.respondTo('readSomeSubrecords');
+      expect(client).to.respondTo('readSubrecordAmount');
     });
 
     it('Create client should have at least one parametter', () => {
@@ -30,12 +37,23 @@ describe('subRecordPicker', () => {
     path: [__dirname, '..', 'test-fixtures', 'subRecordPicker']
   });
 
-  async function callback({getFixture, method, sruUrl, retrieveAll, recordId}) {
+  async function callback({getFixture, method, sruUrl, retrieveAll, recordId, expectedAmount = undefined}) {
     const client = createSubrecordPicker(sruUrl, retrieveAll);
     const expectedRecords = getFixture({components: ['expected-records.json'], reader: READERS.JSON});
-    const {records} = await client[method](recordId);
+    debug(`Testing: ${method}`);
+    const result = await client[method](recordId);
+    debug(`${JSON.stringify(result)}`);
+    const {records, amount} = result;
 
-    expect(format()).to.eql(expectedRecords);
+    // eslint-disable-next-line functional/no-conditional-statements
+    if (expectedRecords) {
+      expect(format()).to.eql(expectedRecords);
+    }
+
+    // eslint-disable-next-line functional/no-conditional-statements
+    if (expectedAmount) {
+      expect(amount).to.eql(expectedAmount);
+    }
 
     function format() {
       return records.map(r => r.toObject());
